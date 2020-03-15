aa5765b7319311cf3678f05a0aa72e996cec7b14
Dan Reeves <hey@danreev.es>
2020-03-15 20:08:31 +0000

    fix flower spawning

--- World.gd
+++ World.gd
@@ -6,8 +6,16 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(dt):
-	if find_node("Flower", true):
-		pass
+	if find_node("Flower", true, false) != null:
+		return
 	else:
 		var flower = Flower.instance()
 		add_child(flower)
+		flower.set_name("Flower")
+		var x_extents = 35
+		var z_extents = 30
+		flower.translation = Vector3(
+			rand_range(-x_extents, x_extents), 
+			0.5, 
+			rand_range(-z_extents + 10, z_extents) # +10 fudge for ortho camera
+		)
