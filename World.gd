be8c0ff49e72ea207d11006b0200b0c8036feb0b
Dan Reeves <hey@danreev.es>
2020-03-16 09:15:46 +0000

    lint

--- World.gd
+++ World.gd
@@ -5,7 +5,7 @@ var Flower = load("res://Flower.tscn")
 func _ready():
 	pass # Replace with function body.
 
-func _process(dt):
+func _process(_dt):
 	if find_node("Flower", true, false) != null:
 		return
 	else:
